59c7b10eff0876e33cb83b3c8db9879a99c5ffbe
golam mostafa <gmnayeem25@gmail.com>
2023-10-21 18:03:13 +0600

    start react app

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+  return (
+    <div>
+      <button
+        className="
+        bg-green-600 hover:rounded-sm transition-all duration-300 ease-in-out 
+        cursor-pointer text-white p-4 m-4 rounded-full"
+      >
+        React App
+      </button>
+    </div>
+  )
+}
+
+export default App
